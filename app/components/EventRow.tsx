db7d8e502a121801c982b3add9fd1b08c4bb8810
Pablo del Barrio <pablodbb@gmail.com>
2023-04-04 20:50:00 +0200

    fixed navbar

--- app/components/EventRow.tsx
+++ app/components/EventRow.tsx
@@ -8,16 +8,17 @@ interface Props {
 
 const EventRow = ({ event }: Props) => {
   return (
-    <div className="bg-gray-300 rounded-lg shadow-lg overflow-hidden mb-2">
+    <div className="bg-gray-300 rounded-lg shadow-lg overflow-hidden mb-3">
       <div className="flex">
         <div className="w-3/4 p-4">
           <div className="flex justify-between mb-2">
             <div className="w-1/2">
               <p className="font-bold text-gray-700">{event.date}</p>
-              {/* <p className="text-gray-600">21:00h</p> */}
             </div>
             <div className="w-1/2 text-right">
-              <p className="text-2xl font-bold text-gray-700">{event.name}</p>
+              <p className="md:text-2xl font-bold text-gray-700">
+                {event.name}
+              </p>
               <p className="text-gray-600"></p>
             </div>
           </div>
---
+++ app/components/NavBar.tsx
@@ -0,0 +1,19 @@
+// "use client";
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="bg-gray-500 p-7 flex justify-center items-center">
+      <div className="mr-20 md:mr-40">
+        <Link href="/" className="font-bold text-gray-800 text-2xl">
+          @bcn
+        </Link>
+      </div>
+      <div className="ml-10 md:ml-40">
+        <button className="bg-gray-300 text-gray-800 font-bold p-2 px-4 rounded">
+          add event
+        </button>
+      </div>
+    </nav>
+  );
+}
