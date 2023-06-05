020ce6c3203d065bc02a5548039967887fd71fbf
Pablo del Barrio <pablodbb@gmail.com>
2023-06-05 18:04:29 +0200

    testeando PWA

--- next.config.js
+++ next.config.js
@@ -1,14 +1,33 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+// };
+
+// const withPWA = require("@imbios/next-pwa");
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   },
+// });
+
+// module.exports = withPWA({});
+
+// module.exports = nextConfig;
+const withPWA = require("@imbios/next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 };
 
-const withPWA = require("@imbios/next-pwa")({
-  dest: "public",
-});
-
-module.exports = withPWA({});
-
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
