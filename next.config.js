30d5c5c4e994abb5a57c668a006c13175de3d3c5
Pablo del Barrio <pablodbb@gmail.com>
2023-06-08 19:17:12 +0200

    testin PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,42 @@
+// const withPWA = require("@imbios/next-pwa")({
+//   dest: "public",
+// });
+
+// /** @type {import("next").NextConfig} */
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   experimental: {
+//     appDir: true,
+//   },
+// });
+
+// const runtimeCaching = require("@imbios/next-pwa/cache");
+
+// const withPWA = require("@imbios/next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+//   runtimeCaching,
+// });
+
+// /** @type {import('next').NextConfig}*/
+// const nextConfig = {};
+
+// module.exports = withPWA(nextConfig);
+
+// TESTING
+
+// /** @type {import('next').NextConfig}*/
+// const nextConfig = {};
+
+const runtimeCaching = require("@imbios/next-pwa/cache");
+
 const withPWA = require("@imbios/next-pwa")({
   dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: false,
 });
 
 /** @type {import("next").NextConfig} */
@@ -9,3 +46,5 @@ module.exports = withPWA({
     appDir: true,
   },
 });
+
+// module.exports = withPWA(nextConfig);
