6d9b473532cd0cf08c31e0a45bd98e4b712c18f5
Pablo del Barrio <pablodbb@gmail.com>
2023-07-09 16:31:44 +0200

    testing PWA

--- next.config.js
+++ next.config.js
@@ -1,42 +1,5 @@
-// const withPWA = require("@imbios/next-pwa")({
-//   dest: "public",
-// });
-
-// /** @type {import("next").NextConfig} */
-// module.exports = withPWA({
-//   reactStrictMode: true,
-//   experimental: {
-//     appDir: true,
-//   },
-// });
-
-// const runtimeCaching = require("@imbios/next-pwa/cache");
-
-// const withPWA = require("@imbios/next-pwa")({
-//   dest: "public",
-//   register: true,
-//   skipWaiting: true,
-//   runtimeCaching,
-// });
-
-// /** @type {import('next').NextConfig}*/
-// const nextConfig = {};
-
-// module.exports = withPWA(nextConfig);
-
-// TESTING
-
-// /** @type {import('next').NextConfig}*/
-// const nextConfig = {};
-
-const runtimeCaching = require("@imbios/next-pwa/cache");
-
 const withPWA = require("@imbios/next-pwa")({
   dest: "public",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  disable: false,
 });
 
 /** @type {import("next").NextConfig} */
@@ -47,4 +10,22 @@ module.exports = withPWA({
   },
 });
 
-// module.exports = withPWA(nextConfig);
+// const runtimeCaching = require("@imbios/next-pwa/cache");
+
+// const withPWA = require("@imbios/next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+//   runtimeCaching,
+//   disable: false,
+// });
+
+// /** @type {import("next").NextConfig} */
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   experimental: {
+//     appDir: true,
+//   },
+// });
+
+// // module.exports = withPWA(nextConfig);
